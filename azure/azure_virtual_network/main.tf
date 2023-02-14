9433922e8d8a930efc56b255293aabb3a810e2db
neilgri@gmail.com <neilgri@gmail.com>
2023-02-14 13:00:06 +0100

    Update main.tf

--- azure/azure_virtual_network/main.tf
+++ azure/azure_virtual_network/main.tf
@@ -22,7 +22,6 @@ resource "azurerm_virtual_network" "vnet" {
   resource_group_name = azurerm_resource_group.rg.name # By referencing the azurerm value it creates an implicit dependancy to the RG being created first
   location            = azurerm_resource_group.rg.location
   address_space       = ["10.123.0.0/16"]
-  dns_servers
 
   tags = local.tags
 }
