4f484695a6974c084e35343b38974d1416df2534
neilgri@gmail.com <neilgri@gmail.com>
2023-02-14 13:21:03 +0100

    address_space

--- azure/azure_virtual_network/main.tf
+++ azure/azure_virtual_network/main.tf
@@ -21,7 +21,7 @@ resource "azurerm_virtual_network" "vnet" {
   name                = "vnet-${var.subscription_name}"
   resource_group_name = azurerm_resource_group.rg.name # By referencing the azurerm value it creates an implicit dependancy to the RG being created first
   location            = azurerm_resource_group.rg.location
-  address_space       = var.vnet_address_space
+  address_space       = var.address_space
 
   tags = local.tags
 }
