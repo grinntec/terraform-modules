b9f707736f3411b1e975d40dd4a7d8ef1a4672fa
neilgri@gmail.com <neilgri@gmail.com>
2023-02-14 13:25:00 +0100

    Update main.tf

--- azure/azure_virtual_network/main.tf
+++ azure/azure_virtual_network/main.tf
@@ -1,20 +1,10 @@
-/*# Generate random text for a unique storage account name
-resource "random_id" "random_id" {
-  keepers = {
-    # Generate a new ID only when a new resource group is defined
-    resource_group = azurerm_resource_group.rg.name
-  }
-
-  byte_length = 8
-}*/
-
-/*# Create resource group
+# Create resource group
 resource "azurerm_resource_group" "rg" {
   name     = var.rg_name
   location = var.rg_location
 
   tags = local.tags
-}*/
+}
 
 # Create virtual network
 resource "azurerm_virtual_network" "vnet" {
