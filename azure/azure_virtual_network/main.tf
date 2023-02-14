50238981eabb56cd8ff2261e8fb4ad3c19c72a24
neilgri@gmail.com <neilgri@gmail.com>
2023-02-14 13:40:17 +0100

    remove service_endpoints

--- azure/azure_virtual_network/main.tf
+++ azure/azure_virtual_network/main.tf
@@ -21,7 +21,7 @@ resource "azurerm_subnet" "subnet" {
   resource_group_name                           = var.rg_name           
   virtual_network_name                          = "vnet-${var.subscription_name}"
   address_prefixes                              = var.address_prefixes    
-  service_endpoints                             = var.service_endpoints 
+  #service_endpoints                             = var.service_endpoints 
   private_endpoint_network_policies_enabled     = true                  
   private_link_service_network_policies_enabled = true                  
 }
