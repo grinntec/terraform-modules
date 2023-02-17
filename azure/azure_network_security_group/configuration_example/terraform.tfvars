2ab6113be1c0297d4138cc1ea63b7e36be3888da
neilgri@gmail.com <neilgri@gmail.com>
2023-02-17 19:03:12 +0100

    create nsg code

---
+++ azure/azure_network_security_group/configuration_example/terraform.tfvars
@@ -0,0 +1,4 @@
+rg_name           = "networking"
+rg_location       = "west europe"
+environment       = "test"
+subnet_name       = "subnet01"
---
+++ azure/azure_network_security_group/module/main.tf
@@ -0,0 +1,38 @@
+# Create network security group
+resource "azurerm_network_security_group" "nsg" {
+  name                = "nsg"
+  location            = var.rg_location
+  resource_group_name = var.rg_name
+
+  tags = local.tags
+}
+
+# Associate the network security group with the subnet
+resource "azurerm_subnet_network_security_group_association" "nsg-association" {
+  subnet_id                 = var.subnet_name
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+# Create network security group rule
+resource "azurerm_network_security_rule" "nsg-rule" {
+  name                        = "rule01"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "80"
+  source_address_prefix       = "185.203.216.208" # You need to use your connecting system public IP address, use ipchicken.com
+  destination_address_prefix  = "*"
+  resource_group_name         = var.rg_name
+  network_security_group_name = azurerm_network_security_group.nsg.name
+}*/
+////////////////////////////////////////////////////////////////////////////////////////
+// LOCALS
+////////////////////////////////////////////////////////////////////////////////////////
+locals {
+  tags = {
+    env = var.environment
+  }
+}
+
