93435a01b9cbb662e37efb0c43948d6a64f42471
neilgri@gmail.com <neilgri@gmail.com>
2023-02-19 14:32:07 +0100

    Update main.tf

--- azure/azure_load_balancer/module/main.tf
+++ azure/azure_load_balancer/module/main.tf
@@ -13,7 +13,7 @@ resource "azurerm_lb" "lb" {
 
   frontend_ip_configuration {
     name                 = "PublicIPAddress"
-    public_ip_address_id = azurerm_public_ip.example.id
+    public_ip_address_id = azurerm_public_ip.pip_lb.id
   }
   tags = local.tags
 }
