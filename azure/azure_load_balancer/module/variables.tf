2a15f6a0ddf11799f9b8f7e95b7006ec7842d10d
neilgri@gmail.com <neilgri@gmail.com>
2023-02-19 14:31:06 +0100

    Update variables.tf

--- azure/azure_load_balancer/module/variables.tf
+++ azure/azure_load_balancer/module/variables.tf
@@ -1,7 +1,14 @@
 variable "rg_name" {
-  type = string
+  description = "Name of the application"
+  type        = string
 }
 
 variable "rg_location" {
-  type = string
+  description = "Azure location of the resource group"
+  type        = string
+}
+
+variable "environment" {
+  description = "Prod, Test, Dev"
+  type        = string
 }
