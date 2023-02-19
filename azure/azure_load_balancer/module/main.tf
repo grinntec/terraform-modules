73b8beb99b961d5e2ea9b7226e1b3474b49a3c6a
neilgri@gmail.com <neilgri@gmail.com>
2023-02-19 14:25:31 +0100

    create lb module

---
+++ azure/azure_load_balancer/module/main.tf
@@ -0,0 +1,17 @@
+resource "azurerm_public_ip" "pip_lb" {
+  name                = "PublicIPForLB"
+  location            = var.rg_location
+  resource_group_name = var.rg_name
+  allocation_method   = "Static"
+}
+
+resource "azurerm_lb" "lb" {
+  name                = "LoadBalancer"
+  location            = var.rg_location
+  resource_group_name = var.rg_name
+
+  frontend_ip_configuration {
+    name                 = "PublicIPAddress"
+    public_ip_address_id = azurerm_public_ip.example.id
+  }
+}
