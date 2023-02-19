120f21bad8f39503b01f2d4cfae3aa15d1f6442e
neilgri@gmail.com <neilgri@gmail.com>
2023-02-19 14:30:04 +0100

    add tags

--- azure/azure_load_balancer/module/main.tf
+++ azure/azure_load_balancer/module/main.tf
@@ -3,6 +3,7 @@ resource "azurerm_public_ip" "pip_lb" {
   location            = var.rg_location
   resource_group_name = var.rg_name
   allocation_method   = "Static"
+  tags                = local.tags
 }
 
 resource "azurerm_lb" "lb" {
@@ -13,5 +14,11 @@ resource "azurerm_lb" "lb" {
   frontend_ip_configuration {
     name                 = "PublicIPAddress"
     public_ip_address_id = azurerm_public_ip.example.id
+  }
+  tags = local.tags
+}
+locals {
+  tags = {
+    env = var.environment
   }
 }
