48bc48d7b74ab939865112b348cb591e431e58be
neilgri@gmail.com <neilgri@gmail.com>
2023-02-26 13:49:31 +0100

    Update main.tf

--- azure/stack_linux_apache_server_load_balanced_public/main.tf
+++ azure/stack_linux_apache_server_load_balanced_public/main.tf
@@ -16,6 +16,13 @@ data "azurerm_subnet" "subnet" {
   resource_group_name  = var.rg_name_vnet
 }
 
+// OUTPUTS
+//--------
+output "lb_public_ip_address" {
+  description = "Web Load Balancer Public Address"
+  value = azurerm_public_ip.pip_public_lb.ip_address
+}
+
 // RESOURCES
 //----------
 
